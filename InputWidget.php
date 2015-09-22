2754c78b40bc2ec24c3545e110e1c5de25e976e8
alexeevdv <mail@alexeevdv.ru>
2015-09-22 16:14:58 +0300

    Formating

--- InputWidget.php
+++ InputWidget.php
@@ -4,34 +4,51 @@ namespace alexeevdv\recaptcha;
 
 use \yii\helpers\Html;
 
-class InputWidget extends \yii\widgets\InputWidget
-{
+class InputWidget extends \yii\widgets\InputWidget {
+    
+    /**
+     * HTML input name
+     * @var string
+     */
     public $name = "recaptcha";
 
+    /**
+     * Site key
+     * @var string 
+     */
     public $siteKey;
 
+    /**
+     * Secret key
+     * @var string
+     */
     public $secret;
+    
+    /**
+     * Html attributes
+     * @var array
+     */
+    public $options = [];
 
-    public function init()
-    {
-        parent::init();
-    }
-
-    public function run()
-    {
-        if(empty($this->siteKey))
-        {
-            if (!\yii::$app->has("recaptcha") || empty(\yii::$app->recaptcha->siteKey))
-            {
+    const JS_API = "https://www.google.com/recaptcha/api.js";
+    
+    public function run() {
+        
+        if(empty($this->siteKey)) {
+            if (!\yii::$app->has("recaptcha") || empty(\yii::$app->recaptcha->siteKey)) {
                 throw new \yii\base\InvalidConfigException("`siteKey` param is required");
             }
             $this->siteKey = \yii::$app->recaptcha->siteKey;
         }
 
-        $this->view->registerJsFile("https://www.google.com/recaptcha/api.js");
+        $this->view->registerJsFile(self::JS_API);
+        
+        $options = \yii\helpers\ArrayHelper::merge([
+            "class" => "g-recapthca",
+            "data-sitekey" => $this->siteKey,
+        ], $this->options);
 
-        return Html::tag("div", "", ["class" => "g-recaptcha", "data-sitekey" => $this->siteKey ]);
-
+        return Html::tag("div", "", $options);
     }
 
 }
--- Validator.php
+++ Validator.php
@@ -2,38 +2,36 @@
 
 namespace alexeevdv\recaptcha;
 
-class Validator extends \yii\validators\Validator
-{
+class Validator extends \yii\validators\Validator {
+    
     public $skipOnEmpty = false;
 
+    /**
+     * Secret key
+     * @var string
+     */
     public $secret;
 
-    public function init()
-    {
+    public function init() {
+        
         parent::init();
 
-        if (empty($this->secret))
-        {
-            if (!\yii::$app->has("recaptcha") || empty(\yii::$app->recaptcha->secret))
-            {
+        if (empty($this->secret)) {
+            if (!\yii::$app->has("recaptcha") || empty(\yii::$app->recaptcha->secret)) {
                 throw new \yii\base\InvalidConfigException("`secret` param is required");
             }
             $this->secret = \yii::$app->recaptcha->secret;
         }
 
-        if ($this->message === null)
-        {
+        if ($this->message === null) {
             $this->message = \yii::t("yii", "The verification code is incorrect.");
         }
     }
 
-    protected function validateValue($value)
-    {
-        if (empty($value))
-        {
+    protected function validateValue($value) {
+        if (empty($value)) {
             $value = \yii::$app->request->post("g-recaptcha-response");
-            if (!$value)
-            {
+            if (!$value) {
                 return [$this->message, []];
             }
         }
@@ -46,13 +44,10 @@ class Validator extends \yii\validators\Validator
 
         $response = \yii\helpers\Json::decode(file_get_contents($request));
 
-        if (!isset($response['success']))
-        {
+        if (!isset($response['success'])) {
             throw new \yii\base\Exception('Invalid reCAPTCHA verify response.');
         }
 
         return $response['success'] ? null : [$this->message, []];
-    }
-
-    
+    }    
 }
